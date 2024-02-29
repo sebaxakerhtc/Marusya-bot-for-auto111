764adc42348840dea426d3a4c122e16510abe20e
sebaxakerhtc <32651506+sebaxakerhtc@users.noreply.github.com>
2024-03-01 00:38:33 +0200

    Edited by recommendation of @FoxxMD
    https://github.com/Kilvoctu/aiyabot/pull/234/files/a434f2d6ce68f51cf3cb7fdd7c3665031e227610#r1508229379

--- core/stablecog.py
+++ core/stablecog.py
@@ -44,7 +44,9 @@ async def update_progress(event_loop, status_message_task, s, queue_object, trie
             fp=buffer
             file = discord.File(fp, filename)
 
-        ips = round((int(queue_object.steps) - progress_data["state"]["sampling_step"]) / progress_data["eta_relative"], 2)
+        ips = '?'
+        if progress_data["eta_relative"] != 0:
+            ips = round((int(queue_object.steps) - progress_data["state"]["sampling_step"]) / progress_data["eta_relative"], 2)
 
         view = viewhandler.ProgressView()
 
